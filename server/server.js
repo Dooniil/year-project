c0531d321505a18d5cacee5f444386b68f7a7f8f
Daniil <dooniil2@yandex.ru>
2022-08-07 20:17:07 +0300

    some improve

--- server/server.js
+++ server/server.js
@@ -4,23 +4,25 @@ import bodyParser from "body-parser";
 import express from "express";
 import authRouter from "./routers/authRouter.js";
 import sequelize from "./database/connect.js";
+import cors from "cors";
 
 const app = express();
 
 const hostname = process.env.HOST;
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTING
-app.use("/", authRouter);
 
 // DATABASE AND STARTING
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
+    app.use("/", authRouter);
     app.listen(port, hostname, () => {
       console.log("Express server is running");
     });
